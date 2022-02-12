230fc3ca7f7a1b76890c4f787f2f01454d0a23a1
RavenSp <sp.raven@yandex.ru>
2022-02-12 23:56:30 +0300

    add sett

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from core.settings import AUTH_KEY
 
 app = FastAPI(title='Learn title')
 
 
 @app.get('/test')
-async def test_function():
+async def test_function(Auth: str = Header(...)):
+    if Auth != AUTH_KEY:
+        return {
+            'answer':'',
+            'error': {
+                'code': 1,
+                'message': 'Auth failed!'
+            }
+        }
     return {'test': 'maks'}
