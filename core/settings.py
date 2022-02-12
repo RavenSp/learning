b2bd9b97740da12eb71d4456eda4055f8f0e55ed
RavenSp <sp.raven@yandex.ru>
2022-02-13 00:01:02 +0300

    add local settings

--- core/settings.py
+++ core/settings.py
@@ -1,1 +1,2 @@
+from local_settings import SUPER_SECRET
 AUTH_KEY  = '23423423'
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Header
-from core.settings import AUTH_KEY
+from core.settings import AUTH_KEY, SUPER_SECRET
 
 app = FastAPI(title='Learn title')
 
 
 @app.get('/test')
-async def test_function(Auth: str = Header(...)):
+async def test_function(Auth: str = Header(...), Secret: str = Header(None)):
     if Auth != AUTH_KEY:
         return {
             'answer':'',
@@ -14,4 +14,8 @@ async def test_function(Auth: str = Header(...)):
                 'message': 'Auth failed!'
             }
         }
-    return {'test': 'maks'}
+
+    if Secret != SUPER_SECRET:
+        return {'test': 'maks'}
+    else:
+        return {'test': 'unknown!'}
